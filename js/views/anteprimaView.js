a183f6f4125ebbdb88f5d3cd26f99a90155f7a7f
Giacomo Lamonaco <giacomolm@hotmail.it>
2014-05-03 20:50:07 +0200

    Now anteprima size change dinamically respect to its content

--- js/views/anteprimaView.js
+++ js/views/anteprimaView.js
@@ -10,9 +10,9 @@ define(["jquery", "underscore", "backbone", "ractive", "raphaelext", "jel", "fil
     	},
     	    
         initialize: function(){
-            this.paper = Raphael(this.$el[0], 1024, 800);
-            this.panZoom = this.paper.panzoom({ initialZoom: 0, initialPosition: { x: 120, y: 70}, minZoom:-15, maxZoom: 10});
-            this.panZoom.enable();
+            this.paper = Raphael(this.$el[0], 200, 200);
+            //this.panZoom = this.paper.panzoom({ initialZoom: -5, initialPosition: { x: 0, y: 0}, minZoom:-10, maxZoom: 10});
+            //this.panZoom.enable();
 
             $(this).on("exported", this.exportHandler);
             this.sizeout = undefined; //event counter
@@ -26,39 +26,61 @@ define(["jquery", "underscore", "backbone", "ractive", "raphaelext", "jel", "fil
             //(re)initialize the collection that will contains only the graphical element related to the shape
             this.shapes = new Array();
             this.connections = connections;
+            //this.panZoom.disable();
 
-            var currentShapes = Jel.topologicalSort(shapes, connections);
-            this.drawItems(currentShapes);
+            //current shapes need to be sorted in a topological way
+            this.currentShapes = Jel.topologicalSort(shapes, connections);
+            this.drawItems(this.currentShapes);
+
+            //adapting anteprima size to fit its content
+            var anteprimaWidth = 0, anteprimaHeight = 0;
+            for (key in this.shapes) {                
+               var temp_width = this.shapes[key].level+this.shapes[key].attrs.width;
+                if(anteprimaWidth<temp_width){
+                    anteprimaWidth = temp_width;
+                }
+                anteprimaHeight = Math.max(this.getBreadth(key), anteprimaHeight);
+            }
+
+            //if(anteprimaWidth>1024 || anteprimaHeight>800)
+            this.paper.setSize(anteprimaWidth, anteprimaHeight);
+            this.panZoom = this.paper.panzoom({ initialZoom: -10, initialPosition: { x: 0, y: 0}, minZoom:-10, maxZoom: 10});
+            this.panZoom.enable();
+
+
+            this.render();
+            
         },
 
-        //depth is referred to the current node in the xml document
-        //breadth is referred to the composed item into the parent canvas
+        //drawing items in the anteprima section
         //parent contains parent id, if it exists
-        drawItems: function(shapes, depth, breadth, parent){
-            var indepth = depth || 0;
-            var i,temp_breadth=0, curr_breadth = breadth || 0;
+        drawItems: function(shapes, parent){
+            var i;
             for(i=0; i<shapes.length; i++){
 
+                //calculating the offSetX..
                 var level = this.getLevel(shapes.at(i).id) || 0;
-                
+                //.. and the offSetY
+                var breadth = this.getBreadth(shapes.at(i).id, parent);
+                //console.log(shapes.at(i).id, shapes.at(i).height, breadth)
                 //Calculating the width depending the parent position, if it exists
                 if(parent) level = Math.max(parent.attrs.width + parent.level+30, level);
-                var currentText = this.paper.text(level+20, (90*(temp_breadth+curr_breadth+i))+5, shapes.at(i).name + ((shapes.at(i).props && shapes.at(i).props.id) ? ":"+shapes.at(i).props.id : ""));
-                var currentShape = this.paper.image(shapes.at(i).url, level, (90*(temp_breadth+curr_breadth+i))+12, shapes.at(i).width || 86 , shapes.at(i).height || 54);
+                var currentText = this.paper.text(level+20, breadth+5, shapes.at(i).name + ((shapes.at(i).props && shapes.at(i).props.id) ? ":"+shapes.at(i).props.id : ""));
+                var currentShape = this.paper.image(shapes.at(i).url, level, breadth+12, shapes.at(i).width || 86 , shapes.at(i).height || 54);
                 //setting the original id
                 currentShape.id = shapes.at(i).id;
                 //setting the level, indicating the margin left, in order to retrieve it later
                 currentShape.level = level;
-                
+                if(parent) currentShape.parent = parent;
+
                 this.shapes[shapes.at(i).id] = currentShape;
                 //draw connnections betwenn element of the same level and between the element and its father
                 if(parent) this.drawConnections(shapes.at(i).id, parent.id);
                 else this.drawConnections(shapes.at(i).id);
                 if(shapes.at(i).shapes && shapes.at(i).shapes.length>0){
-                    temp_breadth += this.drawItems(shapes.at(i).shapes, indepth+level+1,temp_breadth+curr_breadth+i+1, currentShape);
+                    this.drawItems(shapes.at(i).shapes, currentShape);
                 }
             }
-            return shapes.length+temp_breadth;
         },
 
         //draws connections between current shapeId and all the elements preceeding it, including its parent
@@ -77,13 +99,21 @@ define(["jquery", "underscore", "backbone", "ractive", "raphaelext", "jel", "fil
         getLevel: function(shapeId){
             var i, max = 0;
             for(i=0; i<this.connections.length; i++){
-
                 if(this.connections.at(i).inbound == shapeId){
                     max = Math.max(this.shapes[this.connections.at(i).outbound].attrs.width+this.shapes[this.connections.at(i).outbound].level, max);
-                    //console.log();//this.connections.at(i).outbound].attrs.width+this.shapes[this.connections.at(i).outbound].level);
                 }
             }
             return max+30;
+        },
+
+        getBreadth : function(shapeId){
+            var i, current_breadth = 0;
+
+            for (id in this.shapes) {
+                current_breadth += this.shapes[id].attrs.height + 20; 
+            }
+
+            return current_breadth;
         },
 
         zoomIn: function(e){
